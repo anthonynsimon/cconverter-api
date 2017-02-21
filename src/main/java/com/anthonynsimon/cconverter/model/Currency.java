308e7b765dfd5012589e8f5d77ce6b00deda4040
Anthony Najjar <anthonynsimon@gmail.com>
2017-02-21 13:22:16 +0100

    Change static initializer for known codes

--- src/main/java/com/anthonynsimon/cconverter/model/Currency.java
+++ src/main/java/com/anthonynsimon/cconverter/model/Currency.java
@@ -4,49 +4,20 @@ import com.anthonynsimon.cconverter.model.serializers.CurrencySerializer;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonSerialize(using = CurrencySerializer.class)
 public final class Currency {
 
-    private String currencyCode;
+    private static final HashSet<String> KNOWN_CODES = new HashSet<String>(
+            Arrays.asList("AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK",
+                    "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW",
+                    "MXN", "MYR", "NOK", "NZD", "USD", "PHP", "PLN", "RON", "RUB",
+                    "SEK", "SGD", "THB", "TRY", "ZAR", "EUR"));
 
-    private static final Set<String> KNOWN_CODES = new HashSet<String>() {{
-        add("AUD");
-        add("BGN");
-        add("BRL");
-        add("CAD");
-        add("CHF");
-        add("CNY");
-        add("CZK");
-        add("DKK");
-        add("GBP");
-        add("HKD");
-        add("HRK");
-        add("HUF");
-        add("IDR");
-        add("ILS");
-        add("INR");
-        add("JPY");
-        add("KRW");
-        add("MXN");
-        add("MYR");
-        add("NOK");
-        add("NZD");
-        add("USD");
-        add("PHP");
-        add("PLN");
-        add("RON");
-        add("RUB");
-        add("SEK");
-        add("SGD");
-        add("THB");
-        add("TRY");
-        add("ZAR");
-        add("EUR");
-    }};
+    private String currencyCode;
 
     public Currency(String currencyCode) {
         if (currencyCode != null) {
